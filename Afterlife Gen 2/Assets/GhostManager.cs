65261207b0f485a7f7d0c751ca0e0fdeb86d5657
Lomie02 <dylansmith1844@gmail.com>
2023-11-29 23:23:19 +1100

    Recovered lost code from GPU crash

--- Afterlife Gen 2/Assets/GhostManager.cs
+++ Afterlife Gen 2/Assets/GhostManager.cs
@@ -2,17 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+
+public enum EvidenceTypes
+{
+    SpiritBox = 0,
+    Emf,
+    Writing,
+    LaserProjector,
+    BloodyHandprints,
+    GhostOrb,
+    AudioSensor,
+    FreezingTemps,
+    FloatingObjects,
+    RemPod,
+}
+
+[System.Serializable]
+public struct GhostProfile
+{
+    public string m_GhostName;
+    public EvidenceTypes m_Evidence1;
+    public EvidenceTypes m_Evidence2;
+    public EvidenceTypes m_Evidence3;
+}
 public class GhostManager : MonoBehaviour
 {
+    [SerializeField] GhostProfile[] m_GhostProfiles;
+
     [SerializeField] GameObject m_Ghost;
     [SerializeField] Transform[] m_GhostSpawnLocations;
+
+    [SerializeField] GhostAI m_GhostAi;
     void Start()
     {
         if (PhotonNetwork.IsMasterClient)
         {
             int randomSpawn = Random.Range(0, m_GhostSpawnLocations.Length - 1);
             PhotonNetwork.InstantiateRoomObject(m_Ghost.name, m_GhostSpawnLocations[randomSpawn].position, m_GhostSpawnLocations[randomSpawn].rotation);
+
+            m_GhostAi = FindObjectOfType<GhostAI>();
+            m_GhostAi.SetGhostProfile(Random.Range(0, m_GhostProfiles.Length - 1));
         }
     }
+
+    public GhostProfile GrabGhostProfile(int _index)
+    {
+        return m_GhostProfiles[_index];
+    }
+
 
 }
--- Afterlife Gen 2/Assets/Scripts/Equipment/GhostBox.cs
+++ Afterlife Gen 2/Assets/Scripts/Equipment/GhostBox.cs
@@ -2,17 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
 
 public class GhostBox : MonoBehaviour
 {
+    [Header("General")]
+    [SerializeField] float m_DetectionRange = 10f;
+
+    [Header("Responses")]
+    string[] m_GhostBoxQuestions = new string[] { "Hello", "Speak", "Is Someone Here", "Talk", "Reveal yourself", "Hide and seek" };
+    [SerializeField] ConfidenceLevel m_Confidence = ConfidenceLevel.High;
+    KeywordRecognizer recognizer;
+    [Space]
+
+
     [SerializeField] Text m_2nd;
     [SerializeField] Text m_3rd;
 
     NetworkObject m_MyNetworkData;
+    [SerializeField] GhostAI m_Ghost;
 
+    [SerializeField] bool m_CanGiveResponses = false;
     private void Start()
     {
         m_MyNetworkData = GetComponent<NetworkObject>();
+
+        recognizer = new KeywordRecognizer(m_GhostBoxQuestions, m_Confidence);
+        recognizer.OnPhraseRecognized += Recognizer_OnPhraseRecognized;
+
+        if (m_Ghost)
+        {
+            m_Ghost = FindObjectOfType<GhostAI>();
+            AssignGhost();
+        }
+
+    }
+
+    private void Recognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        float DistanceFromGhost;
+
+        if (transform.parent)
+            DistanceFromGhost = Vector3.Distance(transform.localPosition, m_Ghost.gameObject.transform.position);
+        else
+            DistanceFromGhost = Vector3.Distance(transform.position, m_Ghost.gameObject.transform.position);
+
+        if (DistanceFromGhost <= m_DetectionRange && m_CanGiveResponses)
+        {
+            Debug.Log(args.text);
+            //TODO Give Random Respones
+        }
     }
     void Update()
     {
@@ -23,6 +62,30 @@ public class GhostBox : MonoBehaviour
 
             m_2nd.text = _convertedInt2nd.ToString();
             m_3rd.text = _convertedInt3rd.ToString();
+
+            if (!m_Ghost)
+            {
+                m_Ghost = FindObjectOfType<GhostAI>();
+                AssignGhost();
+            }
         }
     }
+
+    void AssignGhost()
+    {
+        if (m_Ghost.GetGhostProfile().m_Evidence1 == EvidenceTypes.SpiritBox || m_Ghost.GetGhostProfile().m_Evidence2 == EvidenceTypes.SpiritBox || m_Ghost.GetGhostProfile().m_Evidence3 == EvidenceTypes.SpiritBox)
+        {
+            m_CanGiveResponses = true;
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (recognizer != null && recognizer.IsRunning)
+        {
+            recognizer.OnPhraseRecognized -= Recognizer_OnPhraseRecognized;
+            recognizer.Stop();
+        }
+    }
+
 }

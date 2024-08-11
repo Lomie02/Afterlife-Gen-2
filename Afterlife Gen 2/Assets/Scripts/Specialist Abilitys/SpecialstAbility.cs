1ec4cc28dfe45227f7397715878841fcd3f48a7f
Lomie02 <dylansmith1844@gmail.com>
2024-08-11 16:35:52 +1000

    Exterminators Special Ability Added

--- Afterlife Gen 2/Assets/Scripts/Specialist Abilitys/SpecialstAbility.cs
+++ Afterlife Gen 2/Assets/Scripts/Specialist Abilitys/SpecialstAbility.cs
@@ -1,10 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 using UnityEngine.Events;
 using Photon.Pun;
-using System;
 
 public enum SpecialistSelected
 {
@@ -17,10 +15,14 @@ public class SpecialstAbility : MonoBehaviour
 {
     [SerializeField] SpecialistSelected m_SpecialistMode = SpecialistSelected.Exterminator;
     [Space]
+
     [SerializeField] UnityEvent m_OnSpecialistActivated;
+    [SerializeField] UnityEvent m_OnSpecialistEnded;
+
     [SerializeField] Image m_SpecialistIcon;
 
-    float m_SpecialistChargeRate = 0.02f;
+    [Header("Charge Rate")]
+    [SerializeField] float m_SpecialistChargeRate = 0.02f;
     bool m_SpecialistIsReady = false;
     float m_SpecialistAmount = 0f;
 
@@ -28,11 +30,21 @@ public class SpecialstAbility : MonoBehaviour
     Text m_PressToActivate;
     SpecialistIconSwapper m_SpecialistSwapper;
 
-    bool m_IsHealingAura = false;
-
     PlayerController[] m_PlayersInGame;
     float m_AuraTimer = 0;
     float m_AuraDuration = 10;
+
+    // Exterminator Ability
+    [Header("Exterminator Data")]
+    [SerializeField] float m_NightVisionDuration = 10f;
+    float m_NightTimer = 0;
+
+    [SerializeField] Volume m_PlayersVolume;
+    [Space]
+    [SerializeField] VolumeProfile m_NightVisionProfile;
+    [SerializeField] VolumeProfile m_DefaultProfile;
+
+    bool m_AbilityInUse;
 
     private void Start()
     {
@@ -83,6 +95,7 @@ public class SpecialstAbility : MonoBehaviour
     {
         if (m_SpecialistIsReady)
         {
+            m_OnSpecialistActivated.Invoke();
             SpecialistAbilityToUse();
             ResetAbility();
         }
@@ -94,8 +107,14 @@ public class SpecialstAbility : MonoBehaviour
         {
             case SpecialistSelected.Exterminator:
 
+                m_NightTimer = m_NightVisionDuration;
+                m_AbilityInUse = true;
+                m_PlayersVolume.profile = m_NightVisionProfile;
+
                 break;
             case SpecialistSelected.Pharmacist:
+
+                m_AbilityInUse = true;
                 BeginHealingAura();
                 break;
             case SpecialistSelected.Trapper:
@@ -109,8 +128,6 @@ public class SpecialstAbility : MonoBehaviour
 
     void BeginHealingAura()
     {
-        m_IsHealingAura = true;
-
         if (m_PlayersInGame.Length == 0)
         {
             m_PlayersInGame = FindObjectsByType<PlayerController>(FindObjectsSortMode.InstanceID);
@@ -139,26 +156,47 @@ public class SpecialstAbility : MonoBehaviour
 
     private void Update()
     {
-        if (!m_View.IsMine) return;
+        if (!m_View.IsMine || !m_AbilityInUse) return;
 
-        if (m_IsHealingAura)
+        switch (m_SpecialistMode)
         {
-            m_AuraTimer -= Time.deltaTime;
-            if(m_AuraTimer <= 0)
-            {
-                m_IsHealingAura = false;
-                m_AuraTimer = m_AuraDuration;
-            }
+            case SpecialistSelected.Exterminator: // Exterminator Ability Update Loop
 
-            for (int i = 0; i < m_PlayersInGame.Length; i++)
-            {
-                if (Vector3.Distance(m_PlayersInGame[i].transform.position, transform.position) <= 5)
+                m_NightTimer -= Time.deltaTime;
+                if (m_NightTimer <= 0)
                 {
-                    m_PlayersInGame[i].GetComponent<PhotonView>().RPC("RPC_RestoreHealth", RpcTarget.All, 10f * Time.deltaTime);
-                    m_PlayersInGame[i].GetComponent<PhotonView>().RPC("RPC_RestoreSanity", RpcTarget.All, 0.2f * Time.deltaTime);
+                    m_AbilityInUse = false;
+                    m_PlayersVolume.profile = m_DefaultProfile;
+
+                    m_OnSpecialistEnded.Invoke();
+                }
+                break;
+
+            case SpecialistSelected.Pharmacist: // Pharmacist Ability Update
+
+                m_AuraTimer -= Time.deltaTime;
+                if (m_AuraTimer <= 0)
+                {
+                    m_AbilityInUse = false;
+                    m_AuraTimer = m_AuraDuration;
                 }
 
-            }
+                for (int i = 0; i < m_PlayersInGame.Length; i++)
+                {
+                    if (Vector3.Distance(m_PlayersInGame[i].transform.position, transform.position) <= 5)
+                    {
+                        m_PlayersInGame[i].GetComponent<PhotonView>().RPC("RPC_RestoreHealth", RpcTarget.All, 10f * Time.deltaTime);
+                        m_PlayersInGame[i].GetComponent<PhotonView>().RPC("RPC_RestoreSanity", RpcTarget.All, 0.2f * Time.deltaTime);
+                    }
+
+                }
+                break;
+
+            case SpecialistSelected.Trapper:
+                break;
+
+            case SpecialistSelected.Cultist:
+                break;
         }
     }
 

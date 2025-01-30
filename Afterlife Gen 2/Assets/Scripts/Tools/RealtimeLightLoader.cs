aedf6b9ac9bf5c345eee9f6c89f93f84135160bf
Dylan <dylansmith1844@gmail.com>
2025-01-30 21:56:46 +1100

    Texture swaps & optimization passes

---
+++ Afterlife Gen 2/Assets/Scripts/Tools/RealtimeLightLoader.cs
@@ -0,0 +1,61 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
+public class RealtimeLightLoader : MonoBehaviour
+{
+    Light[] m_RealtimeLights;
+
+    float m_MaxDistanceFromCamera = 5f;
+
+    float m_FramesCheckLimit = 5;
+    float m_FramesPassed;
+
+    void Start()
+    {
+        // Collect all Realtime Light data
+
+        if (!GetComponent<PhotonView>().IsMine) this.enabled = false;
+
+        GrabLights();
+
+        StartCoroutine(UpdateShadows());
+    }
+
+    void GrabLights()
+    {
+        m_RealtimeLights = FindObjectsByType<Light>(FindObjectsSortMode.None);
+
+    }
+
+    private IEnumerator UpdateShadows()
+    {
+        while (true)
+        {
+            foreach (Light light in m_RealtimeLights)
+            {
+                if (!light) continue;
+
+                float distanceFromCamera = Vector3.Distance(transform.position, light.transform.position);
+                if (distanceFromCamera <= m_MaxDistanceFromCamera)
+                {
+                    light.shadows = LightShadows.Soft;
+
+                    float smoothFactor = Mathf.Clamp01((m_MaxDistanceFromCamera - distanceFromCamera) / m_MaxDistanceFromCamera);
+                    light.shadowStrength = Mathf.Lerp(0, 1, smoothFactor);
+                }
+                else
+                {
+                    light.gameObject.SetActive(distanceFromCamera <= m_MaxDistanceFromCamera + 5 ? true : false);
+                }
+                yield return new WaitForSeconds(0.5f);
+            }
+
+        }
+
+    }
+}

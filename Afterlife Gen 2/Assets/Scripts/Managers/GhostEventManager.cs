10de79743f692f13a79d85932ba89594e3ad940a
Dylan <dylansmith1844@gmail.com>
2025-02-22 19:48:46 +1100

    Specialist System rework

---
+++ Afterlife Gen 2/Assets/Scripts/Managers/GhostEventManager.cs
@@ -0,0 +1,84 @@
+using Photon.Pun;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using Photon.Realtime;
+using Unity.VisualScripting;
+
+public class GhostEventManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] UniversalEvent[] m_GhostEvents;
+    float m_GhostEventRefreshRate;
+    float m_GhostEventRefreshTimer;
+    private void Start()
+    {
+        m_GhostEvents = GetComponentsInChildren<UniversalEvent>();
+
+        for (int i = 0; i < m_GhostEvents.Length; i++)
+        {
+            m_GhostEvents[i].gameObject.SetActive(false);
+        }
+
+        m_GhostEventRefreshRate = Random.Range(10, 25);
+
+        if (PhotonNetwork.IsMasterClient)
+            SetUpRandomEvents();
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        base.OnPlayerLeftRoom(otherPlayer);
+
+    }
+
+    public void Update()
+    {
+        if (!PhotonNetwork.IsMasterClient) return;
+
+        m_GhostEventRefreshTimer -= Time.deltaTime;
+
+        if (m_GhostEventRefreshTimer <= 0)
+        {
+            RefreshEvents();
+            m_GhostEventRefreshRate = Random.Range(10, 25);
+        }
+    }
+
+    public void ClearEvents()
+    {
+        if (!PhotonNetwork.IsMasterClient) return;
+        for (int i = 0; i < m_GhostEvents.Length; i++)
+        {
+            m_GhostEvents[i].ClearEvent();
+        }
+    }
+
+    public void SetUpRandomEvents()
+    {
+        if (!PhotonNetwork.IsMasterClient) return;
+
+        for (int i = 0; i < m_GhostEvents.Length; i++)
+        {
+            if (Random.Range(0, 1) == 1)
+            {
+                m_GhostEvents[i].RestoreEvent();
+            }
+        }
+    }
+
+    public void RefreshEvents()
+    {
+        if (!PhotonNetwork.IsMasterClient) return;
+
+        for (int i = 0; i < m_GhostEvents.Length; i++)
+        {
+            m_GhostEvents[i].ClearEvent();
+
+            if (Random.Range(0, 1) == 1)
+            {
+                m_GhostEvents[i].RestoreEvent();
+            }
+        }
+    }
+
+}
